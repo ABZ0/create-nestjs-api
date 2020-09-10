6b4644959367985411f877c629899549bd82134a
Abdallah Hemedah <hemedah94@gmail.com>
2020-09-10 23:51:36 +0200

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  return console.log(`\x1b[31m
+  Invalid directory name.
+  Usage: create-nestjs-api name-of-api
+`);
+}
+
+const repoURL = 'https://github.com/ABZ0/nestjs-starter.git';
+
+runCommand('git', ['clone', '--depth=1', repoURL, name])
+  .then(() => {
+    return runCommand('rm', ['-rf', `${name}/.git`]);
+  }).then(() => {
+    console.log('Installing dependencies...');
+    return runCommand('npm', ['install'], {
+      cwd: process.cwd() + '/' + name
+    });
+  }).then(() => {
+    console.log('\x1b[32mDone! 🏁');
+  });
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    spawned.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
